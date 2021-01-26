edf89e7411c819ba7088a4c5fde46d687a66762d
JackyMao1999 <48790075+JackyMao1999@users.noreply.github.com>
2021-01-26 18:39:48 +0800

    Update robot_broadcaster.cpp

--- src/robot_broadcaster.cpp
+++ src/robot_broadcaster.cpp
@@ -2,9 +2,11 @@
 #include <std_msgs/String.h>
 #include <sensor_msgs/NavSatFix.h>
 #include <sensor_msgs/Imu.h>
+#include <nav_msgs/Odometry.h>
+#include <rosgraph_msgs/Clock.h>
 #include <tf/transform_broadcaster.h>
 #include "ros/ros.h"
- 
+
 #include <webots_ros/set_float.h>
 #include <webots_ros/set_int.h>
 #include <webots_ros/Int32Stamped.h>
@@ -23,9 +25,13 @@ static vector<string> controllerList;
 ros::ServiceClient timeStepClient;          //时钟通讯客户端
 webots_ros::set_int timeStepSrv;            //时钟服务数据
 
-double GPSvalues[3];                        // gps数据
-double Inertialvalues[4];                   // inertial_unit数据
+ros::Publisher odompub;
 
+double GPSvalues[6];                        // gps数据
+int gps_flag=1;
+double Inertialvalues[4];                   // inertial_unit数据
+double liner_speed=0;
+double angular_speed=0;
 /*******************************************************
 * Function name ：controllerNameCallback
 * Description   ：控制器名回调函数，获取当前ROS存在的机器人控制器
@@ -60,7 +66,7 @@ void broadcastTransform()
 {
     static tf::TransformBroadcaster br;
     tf::Transform transform;
-    transform.setOrigin(tf::Vector3(-GPSvalues[2],GPSvalues[0],GPSvalues[1]));// 设置原点
+    transform.setOrigin(tf::Vector3(-(GPSvalues[2]-GPSvalues[5]),GPSvalues[0]-GPSvalues[3],GPSvalues[1]));// 设置原点
     tf::Quaternion q(Inertialvalues[0],Inertialvalues[1],Inertialvalues[2],Inertialvalues[3]);
     q = q.inverse();
     transform.setRotation(q);
@@ -70,11 +76,41 @@ void broadcastTransform()
 
 }
 
+void send_odom_data()
+{
+    nav_msgs::Odometry odom;
+    odom.header.frame_id = "odom";
+    odom.child_frame_id = "base_link";
+    odom.header.stamp = ros::Time::now();
+    odom.pose.pose.position.x = -(GPSvalues[2]-GPSvalues[5]);
+    odom.pose.pose.position.y = GPSvalues[0]-GPSvalues[3];
+    odom.pose.pose.position.z = 0;
+
+    odom.pose.pose.orientation.x = 0;
+    odom.pose.pose.orientation.y = 0;
+    odom.pose.pose.orientation.z = Inertialvalues[2];
+    odom.pose.pose.orientation.w = -Inertialvalues[3];
+
+    odom.twist.twist.linear.x = liner_speed;
+    odom.twist.twist.angular.z = angular_speed;
+
+    odompub.publish(odom);
+
+}
+
+
 void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr &values)
 {
     GPSvalues[0] = values->latitude;
     GPSvalues[1] = values->altitude;
     GPSvalues[2] = values->longitude;
+    if (gps_flag)
+    {
+        GPSvalues[3] = values->latitude;
+        GPSvalues[4] = values->altitude;
+        GPSvalues[5] = values->longitude;
+        gps_flag=0;
+    }
     broadcastTransform();
 }
 
@@ -84,9 +120,15 @@ void inertial_unitCallback(const sensor_msgs::Imu::ConstPtr &values)
     Inertialvalues[1] = values->orientation.y;
     Inertialvalues[2] = values->orientation.z;
     Inertialvalues[3] = values->orientation.w;
+    
     broadcastTransform();
 }
-
+void velCallback(const nav_msgs::Odometry::ConstPtr &value)
+{
+    liner_speed = value->twist.twist.linear.x;
+    angular_speed = value->twist.twist.angular.z;
+    send_odom_data();
+}
 
 int main(int argc, char **argv) {
    
@@ -181,12 +223,21 @@ int main(int argc, char **argv) {
     ROS_INFO("You can now start the creation of the map using 'rosrun gmapping slam_gmapping "
             "scan:=/volcano/Sick_LMS_291/laser_scan/layer0 _xmax:=30 _xmin:=-30 _ymax:=30 _ymin:=-30 _delta:=0.2'.");
     ROS_INFO("You can now visualize the sensors output in rqt using 'rqt'.");
-   // main loop
+
+    ros::Subscriber sub_speed;
+    sub_speed = n->subscribe("/vel", 1, velCallback);
+    ros::Publisher time = n->advertise<rosgraph_msgs::Clock>("/clock",10);//订阅/clock话题
+    odompub = n->advertise<nav_msgs::Odometry>("/odom",10);
+
+    // main loop
     while (ros::ok()) {
         if (!timeStepClient.call(timeStepSrv) || !timeStepSrv.response.success) {
         ROS_ERROR("Failed to call service time_step for next step.");
         break;
         }
+        rosgraph_msgs::Clock clock;
+        clock.clock = ros::Time::now();
+        time.publish(clock);//实时发布rosgraph_msgs::Clock信息
         ros::spinOnce();
     }
     
