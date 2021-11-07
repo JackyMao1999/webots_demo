81b4d79a4f303146be5ba702ea4276ac9c61d04a
JackyMao <837002144@qq.com>
2021-11-07 21:02:28 +0800

    dev webots_ros.h

---
+++ include/webots_ros.h
@@ -0,0 +1,132 @@
+/************************************************* 
+Copyright:Webots_ros API
+Author: 锡城筱凯
+Date:2021-11-07 
+Blog：https://blog.csdn.net/xiaokai1999
+Description:Webots_ros官方库的整合库，简便易用
+**************************************************/  
+#ifndef _WEBOTS_ROS_H
+#define _WEBOTS_ROS_H
+
+
+#include "ros/ros.h"
+#include <std_msgs/String.h>
+#include <webots_ros/set_int.h>
+#include <webots_ros/set_float.h>
+#include <webots_ros/Int32Stamped.h>
+class Webots
+{
+private:
+    // 同步Webots所设置的时钟
+    int TIME_STEP;
+    // 机器人的名称，对应Webots中Robot下的name属性
+    std::string ROBOT_NAME;
+public:
+    // 时钟通讯service客户端
+    static ros::ServiceClient timeStepClient;  
+    // 时钟服务数据        
+    static webots_ros::set_int timeStepSrv;            
+    // Webots类构造函数，设置必须变量
+    Webots(int TIME_STEP,std::string ROBOT_NAME);
+    // 初始化Webots和Ros之间的桥梁，成功返回0，失败返回1
+    int Init(ros::NodeHandle *n, ros::Subscriber nameSub, const int &controllerCount, const std::vector<std::string> &controllerList);
+    // 初始化电机，成功返回0，失败返回1
+    int InitMotors(ros::NodeHandle *n, const char *motorNames[], int NMOTORS); 
+    // 设置电机位置属性，成功返回0，失败返回1
+    int SetMotorsPositon(ros::NodeHandle *n, const char *motorNames, float value);
+    // 设置电机速度属性，成功返回0，失败返回1
+    int SetMotorsVelocity(ros::NodeHandle *n, const char *motorNames, float value);
+    // 使能Webots服务，成功返回0，失败返回1
+    int EnableService(ros::NodeHandle *n, std::string Service_name);
+    int ChecktimeStep();
+    int Servicesub();
+    // 退出函数
+    void Quit();
+};
+Webots::Webots(int TIME_STEP,std::string ROBOT_NAME){
+    this->TIME_STEP = TIME_STEP;
+    this->ROBOT_NAME = ROBOT_NAME;
+}
+int Webots::Init(ros::NodeHandle *n, ros::Subscriber nameSub, const int &controllerCount, const std::vector<std::string> &controllerList){
+    std::string controllerName;
+    // 订阅webots中所有可用的model_name
+    while (controllerCount == 0 || controllerCount < nameSub.getNumPublishers()) {
+        ROS_INFO("controllerCount=%d",controllerCount);
+        ros::spinOnce();
+    }
+    
+    // 服务订阅time_step和webots保持同步
+    timeStepClient = n->serviceClient<webots_ros::set_int>("robot/robot/time_step");
+    timeStepSrv.request.value = TIME_STEP;
+
+    // 如果在webots中有多个控制器的话，需要让用户选择一个控制器
+    if (controllerCount == 1)
+        controllerName = controllerList[0];
+    else {
+        int wantedController = 0;
+        std::cout << "Choose the # of the controller you want to use:\n";
+        std::cin >> wantedController;
+        if (1 <= wantedController && wantedController <= controllerCount)
+            controllerName = controllerList[wantedController - 1];
+        else {
+            ROS_ERROR("Invalid number for controller choice.");
+            return 1;
+        }
+    }
+    ROS_INFO("Using controller: '%s'", controllerName.c_str());
+    // 退出主题，因为已经不重要了
+    nameSub.shutdown();
+}
+int Webots::InitMotors(ros::NodeHandle *n, const char *motorNames[], int NMOTORS){
+    for (int i = 0; i < NMOTORS; ++i) {
+        SetMotorsPositon(n, motorNames[i], INFINITY);
+        SetMotorsVelocity(n,motorNames[i], 0.0);
+    }
+}
+int Webots::SetMotorsPositon(ros::NodeHandle *n, const char *motorNames, float value){
+    ros::ServiceClient set_position_client;     // 电机位置通讯service客户端
+    webots_ros::set_float set_position_srv;     // 电机位置服务数据
+    set_position_client = n->serviceClient<webots_ros::set_float>(ROBOT_NAME + "/" + std::string(motorNames) + std::string("/set_position"));   
+    set_position_srv.request.value = value;
+    if (set_position_client.call(set_position_srv) && set_position_srv.response.success){
+        ROS_INFO("Position set to %f for motor %s.", value, motorNames);   
+        return 0;
+    }   
+    else{
+        ROS_ERROR("Failed to call service set_position on motor %s.", motorNames);
+        return 1;
+    }
+}
+int Webots::SetMotorsVelocity(ros::NodeHandle *n, const char *motorNames, float value){
+    ros::ServiceClient set_velocity_client;     // 电机速度通讯service客户端
+    webots_ros::set_float set_velocity_srv;     // 电机速度服务数据 
+    set_velocity_client = n->serviceClient<webots_ros::set_float>(ROBOT_NAME + "/" + std::string(motorNames) + std::string("/set_velocity"));   
+    set_velocity_srv.request.value = value;   
+    if (set_velocity_client.call(set_velocity_srv) && set_velocity_srv.response.success == 1)     
+        ROS_INFO("Velocity set to %f for motor %s.",value, motorNames);   
+    else     
+        ROS_ERROR("Failed to call service set_velocity on motor %s.", motorNames); 
+}
+int Webots::EnableService(ros::NodeHandle *n, std::string Service_name){
+    ros::ServiceClient EnableClient;
+    webots_ros::set_int Enablesrv;
+   
+    EnableClient = n->serviceClient<webots_ros::set_int>(ROBOT_NAME + "/" + Service_name + "/enable");
+    Enablesrv.request.value = TIME_STEP;
+    if (EnableClient.call(Enablesrv) && Enablesrv.response.success){
+        ROS_INFO("Enable %s successful", Service_name);
+        return 0;
+    } 
+    else{
+        ROS_ERROR("Could not enable %s, success = %d.", Service_name, Enablesrv.response.success);
+        return 1;
+    }
+}
+void Webots::Quit(){
+    ROS_INFO("Stopped the node.");
+    timeStepSrv.request.value = 0; 
+    timeStepClient.call(timeStepSrv); 
+    ros::shutdown();
+    exit(0);
+}
+#endif
--- src/velocity_keyboard_v2.cpp
+++ src/velocity_keyboard_v2.cpp
@@ -3,43 +3,30 @@ Copyright:Webots Demo
 Author: 锡城筱凯
 Date:2021-06-30 
 Blog：https://blog.csdn.net/xiaokai1999
+Change: 2021-11-07
 Description:Webots Demo 通过webots控制机器人移动
 **************************************************/  
 #include <signal.h>
-#include <std_msgs/String.h>
-#include "ros/ros.h"
 #include <locale.h> 
+#include <webots_ros.h>
 #include <nav_msgs/Odometry.h>
-#include <webots_ros/set_float.h>
-#include <webots_ros/set_int.h>
-#include <webots_ros/Int32Stamped.h>
-using namespace std;
-
-#define TIME_STEP 32                         // 时钟
-#define NMOTORS 2                            // 电机数量
-#define MAX_SPEED 2.0                        // 电机最大速度
-#define ROBOT_NAME "robot/"                  // ROBOT名称 
 
 ros::NodeHandle *n;
+
+const int TIME_STEP = 32;                   // 时钟
+const int NMOTORS = 2;                      // 电机数量
+const float MAX_SPEED = 2.0;                // 电机最大速度
+const std::string ROBOT_NAME = "robot/";    // ROBOT名称 
+double speeds[NMOTORS]={0.0,0.0};           // 电机速度值 0.0～10.0
+float linear_temp=0, angular_temp=0;        // 暂存的线速度和角速度
+
+static const char *motorNames[NMOTORS] ={"left_motor", "right_motor"};// 控制位置电机名称
 
 static int controllerCount;
 static std::vector<std::string> controllerList; 
 
-ros::ServiceClient timeStepClient;          // 时钟通讯service客户端
-webots_ros::set_int timeStepSrv;            // 时钟服务数据
-
-ros::ServiceClient set_velocity_client;     // 电机速度通讯service客户端
-webots_ros::set_float set_velocity_srv;     // 电机速度服务数据
-
-ros::ServiceClient set_position_client;     // 电机位置通讯service客户端
-webots_ros::set_float set_position_srv;     // 电机位置服务数据
-
 ros::Publisher pub_speed;                   // 发布 /vel
-
-double speeds[NMOTORS]={0.0,0.0};           // 电机速度值 0～10
-float linear_temp=0, angular_temp=0;        // 暂存的线速度和角速度
-
-static const char *motorNames[NMOTORS] ={"left_motor", "right_motor"};// 控制位置电机名称
+Webots w = Webots(TIME_STEP,ROBOT_NAME);
 
 /*******************************************************
 * Function name ：updateSpeed
@@ -55,9 +42,7 @@ void updateSpeed() {
     speeds[1]  = 10.0*(2.0*linear_temp + L*angular_temp)/2.0;
     for (int i = 0; i < NMOTORS; ++i) {
         // 更新速度
-        set_velocity_client = n->serviceClient<webots_ros::set_float>(string(ROBOT_NAME) + string(motorNames[i]) + string("/set_velocity"));   
-        set_velocity_srv.request.value = -speeds[i];
-        set_velocity_client.call(set_velocity_srv);
+        w.SetMotorsVelocity(n, motorNames[i], -speeds[i]);
     }
     speed_data.header.stamp = ros::Time::now();
     speed_data.twist.twist.linear.x = linear_temp;
@@ -80,20 +65,7 @@ void controllerNameCallback(const std_msgs::String::ConstPtr &name) {
     ROS_INFO("Controller #%d: %s.", controllerCount, controllerList.back().c_str());
 }
 
-/*******************************************************
-* Function name ：quit
-* Description   ：退出函数
-* Parameter     ：
-        @sig   信号
-* Return        ：无
-**********************************************************/
-void quit(int sig) {
-    ROS_INFO("User stopped the '/robot' node.");
-    timeStepSrv.request.value = 0; 
-    timeStepClient.call(timeStepSrv); 
-    ros::shutdown();
-    exit(0);
-}
+
 /*******************************************************
 * Function name ：键盘返回函数
 * Description   ：当键盘动作，就会进入此函数内
@@ -130,7 +102,20 @@ void keyboardDataCallback(const webots_ros::Int32Stamped::ConstPtr &value)
     }
 }
 
-
+/*******************************************************
+* Function name ：quit
+* Description   ：退出函数
+* Parameter     ：
+        @sig   信号
+* Return        ：无
+**********************************************************/
+void quit(int sig) {
+    ROS_INFO("User stopped the '/robot' node.");
+    timeStepSrv.request.value = 0; 
+    timeStepClient.call(timeStepSrv); 
+    ros::shutdown();
+    exit(0);
+}
 int main(int argc, char **argv) {
     std::string controllerName;
     // 在ROS网络中创建一个名为robot_init的节点
@@ -140,62 +125,14 @@ int main(int argc, char **argv) {
     signal(SIGINT, quit);
 
     // 订阅webots中所有可用的model_name
-    ros::Subscriber nameSub = n->subscribe("model_name", 100, controllerNameCallback);
-    while (controllerCount == 0 || controllerCount < nameSub.getNumPublishers()) {
-        ros::spinOnce();
-    }
-    ros::spinOnce();
-    // 服务订阅time_step和webots保持同步
-    timeStepClient = n->serviceClient<webots_ros::set_int>("robot/robot/time_step");
-    timeStepSrv.request.value = TIME_STEP;
-
-    // 如果在webots中有多个控制器的话，需要让用户选择一个控制器
-    if (controllerCount == 1)
-        controllerName = controllerList[0];
-    else {
-        int wantedController = 0;
-        std::cout << "Choose the # of the controller you want to use:\n";
-        std::cin >> wantedController;
-        if (1 <= wantedController && wantedController <= controllerCount)
-            controllerName = controllerList[wantedController - 1];
-        else {
-            ROS_ERROR("Invalid number for controller choice.");
-        return 1;
-        }
-    }
-    ROS_INFO("Using controller: '%s'", controllerName.c_str());
-    // 退出主题，因为已经不重要了
-    nameSub.shutdown();
-
-    //初始化电机
-    for (int i = 0; i < NMOTORS; ++i) {
-        // position速度控制时设置为缺省值INFINITY   
-        set_position_client = n->serviceClient<webots_ros::set_float>(string(ROBOT_NAME) + string(motorNames[i]) + string("/set_position"));   
-        set_position_srv.request.value = INFINITY;
-        if (set_position_client.call(set_position_srv) && set_position_srv.response.success)     
-            ROS_INFO("Position set to INFINITY for motor %s.", motorNames[i]);   
-        else     
-            ROS_ERROR("Failed to call service set_position on motor %s.", motorNames[i]);
-        // velocity初始速度设置为0   
-        set_velocity_client = n->serviceClient<webots_ros::set_float>(string(ROBOT_NAME) + string(motorNames[i]) + string("/set_velocity"));   
-        set_velocity_srv.request.value = 0.0;   
-        if (set_velocity_client.call(set_velocity_srv) && set_velocity_srv.response.success == 1)     
-            ROS_INFO("Velocity set to 0.0 for motor %s.", motorNames[i]);   
-        else     
-            ROS_ERROR("Failed to call service set_velocity on motor %s.", motorNames[i]);
-    }   
     
+    ros::Subscriber nameSub = n->subscribe("model_name", 10, controllerNameCallback);
+    w.Init(n, nameSub, controllerCount, controllerList);
+    w.InitMotors(n, motorNames, NMOTORS);
     pub_speed = n->advertise<nav_msgs::Odometry>("/vel",1);
-    // 服务订阅键盘
-    ros::ServiceClient keyboardEnableClient;
-    webots_ros::set_int keyboardEnablesrv;
-   
-    keyboardEnableClient = n->serviceClient<webots_ros::set_int>(string(ROBOT_NAME)+string("keyboard/enable"));
-    keyboardEnablesrv.request.value = TIME_STEP;
-    if (keyboardEnableClient.call(keyboardEnablesrv) && keyboardEnablesrv.response.success)
-    {
+    if(!w.EnableService(n, "keyboard")){
         ros::Subscriber keyboardSub;
-        keyboardSub = n->subscribe(string(ROBOT_NAME)+string("keyboard/key"),1,keyboardDataCallback);
+        keyboardSub = n->subscribe(std::string(ROBOT_NAME)+std::string("keyboard/key"),1,keyboardDataCallback);
         while (keyboardSub.getNumPublishers() == 0) {}
         setlocale(LC_CTYPE,"zh_CN.utf8");//设置中文
         ROS_INFO("Keyboard enabled.");
@@ -208,19 +145,15 @@ int main(int argc, char **argv) {
         while (ros::ok()) {   
             ros::spinOnce();
             updateSpeed();
-            if (!timeStepClient.call(timeStepSrv) || !timeStepSrv.response.success){  
+            if (!Webots::timeStepClient.call(Webots::timeStepSrv) || !Webots::timeStepSrv.response.success){  
                 ROS_ERROR("Failed to call service time_step for next step.");     
                 break;   
             }   
             ros::spinOnce();
         } 
     }
-    else
-    ROS_ERROR("Could not enable keyboard, success = %d.", keyboardEnablesrv.response.success);
-
-    timeStepSrv.request.value = 0;
-    timeStepClient.call(timeStepSrv);
-    ros::shutdown(); 
+    
+    w.Quit();
     return 0;
 
 }
